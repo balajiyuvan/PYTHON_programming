210b7552eda884a6b3a3531ecb0c53c0ce90b4a6
balajiyuvan <36121216+balajiyuvan@users.noreply.github.com>
2018-02-04 15:06:39 +0530

    Add files via upload

---
+++ sring.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------
+balaji="yuvan ram"
+l= balaji.count("a")
+print l
+loc = balaji.find('v')
+print l
+balaji=balaji.lower()
+print balaji
+balaji=balaji.upper()
+print balaji
+balaji=balaji.replace('a', 'k')
+print balaji
+balaji=balaji.strip()
+print balaji
