bdbaa4acd2b4472a96a79e4ebca2a4225d8c56b5
balajiyuvan <36121216+balajiyuvan@users.noreply.github.com>
2018-02-11 00:16:29 +0530

    Add files via upload

---
+++ code_katta/odd_or_even.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      balaji yuvan
+#
+# Created:     10-02-2018
+# Copyright:   (c) balaji yuvan 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+b=input()%2
+if(b==1):
+    print("number is odd")
+else:
+    print("number is even")
